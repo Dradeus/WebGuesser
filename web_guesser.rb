f10c7357cf6a2017568b6f06a02db29d6474563a
Dradeus <dradeus@gmail.com>
2016-09-02 20:27:08 -0400

    I4: implemented guess limiting

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,31 +1,48 @@
 require 'sinatra'
 require "sinatra/reloader"
 
-SECRET_NUMBER = rand(100)
+@@secret_number = rand(100)
+@@number_of_guesses = 5
 
 get '/' do
-	guess = params["guess"].to_i
-	message = check_guess(guess)
-	erb :index, :locals => {:number => SECRET_NUMBER,
-												  :message => message}
+	if params["guess"].to_s.empty?
+		
+	message = "please input a guess"
+		erb :index, :locals => {:number => @@secret_number,
+												  :message => message, :guesses => @@number_of_guesses}
+	else 
+		@@number_of_guesses -= 1
+		guess = params["guess"].to_i
+		message = check_guess(guess)
+		params["guess"] = ""
+		erb :index, :locals => {:number => @@secret_number,
+												  :message => message, :guesses => @@number_of_guesses}
+	end
 end
 
 def check_guess(guess)
-	if guess > SECRET_NUMBER
-		if guess > SECRET_NUMBER+5
+	if @@number_of_guesses == 0
+		@@secret_number = rand(100)
+		@@number_of_guesses = 5
+		message = "You have lost, a new number has been generated"
+	
+	elsif guess > @@secret_number
+		if guess > @@secret_number+5
 			message = "Way too high!"
 		else
 			message = "Too high!"
 		end
 
-	elsif guess < SECRET_NUMBER
-		if guess < SECRET_NUMBER-5
+	elsif guess < @@secret_number
+		if guess < @@secret_number-5
 			message = "Way too low!"
 		else
 			message = "Too low!"
 		end
-		
+
 	else
+		@@secret_number = rand(100)
+		@@number_of_guesses = 5
 		message = "You got it right!"
 	end
 end
